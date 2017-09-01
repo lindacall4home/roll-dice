db4b7143ab1bca0767391fe42a8fe98188575d00
Linda Call <lindacall4home@hotmail.com>
2017-08-31 21:34:45 -0600

    allow animation to replay

--- main.js
+++ main.js
@@ -6,6 +6,13 @@ $(document).ready(function() {
   }
 
   $('.wrapper').click(() => rollDice())
+  $('#dice-container').on("animationend", e => {
+    $('#dice-container').removeClass('roll');
+  });
+  $('.die').on("animationend", e => {
+    $('.die').removeClass('spin');
+  });
+
 
   $('body').snowfall({image :"bower_components/jquery-snowfall/examples/images/flake.png", minSize: 10, maxSize:32});
 
