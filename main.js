0f536f6124aa9a4f64aa26a1689e9325d3a81ab5
Linda Call <lindacall4home@hotmail.com>
2017-08-31 22:05:34 -0600

    make dice roll in different directions

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 $(document).ready(function() {
 
   function rollDice(){
-    $('#dice-container').addClass('roll')
-    $('.die').addClass('spin')
+    $('#dice-container1').addClass('roll1')
+    $('#die1').addClass('spin')
+    $('#dice-container2').addClass('roll2')
+    $('#die2').addClass('spin')
   }
 
   $('.wrapper').click(() => rollDice())
-  $('#dice-container').on("animationend", e => {
-    $('#dice-container').removeClass('roll');
-  });
-  $('.die').on("animationend", e => {
-    $('.die').removeClass('spin');
-  });
 
+  $('.dice-container').on("animationend", e => {
+    $('#dice-container1').removeClass('roll1');
+    $('#die1').removeClass('spin');
+    $('#dice-container2').removeClass('roll2');
+    $('#die2').removeClass('spin');
+  });            
 
   $('body').snowfall({image :"bower_components/jquery-snowfall/examples/images/flake.png", minSize: 10, maxSize:32});
 
