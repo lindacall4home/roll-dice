13be767f0d05fe7ca28a6db4c12c4b1b4c097b09
Linda Call <lindacall4home@hotmail.com>
2017-09-01 19:33:48 -0600

    randomize dice rolls

--- main.js
+++ main.js
@@ -1,21 +1,49 @@
 $(document).ready(function() {
 
   function rollDice(){
-    $('#dice-container1').addClass('roll1')
-    $('#die1').addClass('spin')
-    $('#dice-container2').addClass('roll2')
-    $('#die2').addClass('spin')
+    startDieAnimation($('#die-container1'), 'roll1');
+    startDieAnimation($('#die-container2'), 'roll2');
   }
 
-  $('.wrapper').click(() => rollDice())
+  function startDieAnimation($dieContainer, rollAnimationName){
+    $dieContainer.addClass(rollAnimationName);
+    $dieContainer.children('.die').addClass('spin');
+  }
 
-  $('.dice-container').on("animationend", e => {
-    $('#dice-container1').removeClass('roll1');
-    $('#die1').removeClass('spin');
-    $('#dice-container2').removeClass('roll2');
-    $('#die2').removeClass('spin');
-  });            
+  $('#die-container1').on("animationend", e => {
+    removeAnimationClasses($('#die-container1'), 'roll1');
+    setDieFrontFace($('#die-container1'));
+  });
 
-  $('body').snowfall({image :"bower_components/jquery-snowfall/examples/images/flake.png", minSize: 10, maxSize:32});
+  $('#die-container2').on("animationend", e => {
+    removeAnimationClasses($('#die-container2'), 'roll2');
+    setDieFrontFace($('#die-container2'));
+  });
+
+  function removeAnimationClasses($dieContainer, rollAnimationName){
+    $dieContainer.removeClass(rollAnimationName);
+    $dieContainer.children('.die').removeClass('spin');
+  }
+
+  function setDieFrontFace($dieContainer){
+    let die = $dieContainer.children('.die')[0];
+    $(die).removeClass('show' + die.dataset.front);
+
+    let newFront = Math.floor(Math.random() * 6) + 1;
+    die.dataset.front = newFront;
+    $(die).addClass('show' + newFront);
+  }
+
+  function init(){
+
+    $('body').snowfall({image :"bower_components/jquery-snowfall/examples/images/flake.png", minSize: 10, maxSize:32});
+
+    setDieFrontFace($('#die-container1'));
+    setDieFrontFace($('#die-container2'));
+
+    $('#roll-btn').click(() => rollDice());
+  }
+
+  init();
 
 });
